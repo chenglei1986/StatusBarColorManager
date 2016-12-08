1a142533b6b4c2699a6497f4ac153ec2632bdbd3
chenglei <chenglei.1986@163.com>
2016-12-08 22:08:08 +0800

    add screenshots

---
+++ app/src/main/java/com/github/chenglei1986/statusbar/sample/ColoredStatusBarActivity.java
@@ -0,0 +1,36 @@
+package com.github.chenglei1986.statusbar.sample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.chenglei1986.statusbar.StatusBarColorManager;
+
+public class ColoredStatusBarActivity extends AppCompatActivity {
+
+    private static final String EXTRA_STATUS_BAR_COLOR = "extra_status_bar_color";
+
+    private StatusBarColorManager mStatusBarColorManager;
+
+    public static void actionColoredStatusBar(Context context, @ColorInt int statusBarColor) {
+        Intent intent = new Intent(context, ColoredStatusBarActivity.class);
+        intent.putExtra(EXTRA_STATUS_BAR_COLOR, statusBarColor);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colored_status_bar);
+
+        mStatusBarColorManager = new StatusBarColorManager(this);
+        int statusBarColor = getIntent().getIntExtra(EXTRA_STATUS_BAR_COLOR, Color.BLACK);
+        mStatusBarColorManager.setStatusBarColor(statusBarColor, true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(statusBarColor));
+    }
+}
--- app/src/main/java/com/github/chenglei1986/statusbar/sample/MainActivity.java
+++ app/src/main/java/com/github/chenglei1986/statusbar/sample/MainActivity.java
@@ -1,33 +1,76 @@
 package com.github.chenglei1986.statusbar.sample;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.github.chenglei1986.statusbar.ColorUtil;
-import com.github.chenglei1986.statusbar.StatusBarColorManager;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    private StatusBarColorManager mStatusBarColorManager;
+    private static final String[] TITLES = {"Light", "Dark", "Blue"};
+    private static final int[] COLORS = {0xFFCCCCCC, 0xFF333333, 0xFF3F51B5};
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.random_color_button).setOnClickListener(this);
-
-        mStatusBarColorManager = new StatusBarColorManager(this);
+        mListView = (ListView) findViewById(R.id.listView);
+        mListView.setAdapter(new MyAdapter());
+        mListView.setOnItemClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-        int color = Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
-        mStatusBarColorManager.setStatusBarColor(0xFF333333, true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFF333333));
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        int statusBarColor = COLORS[position];
+        ColoredStatusBarActivity.actionColoredStatusBar(this, statusBarColor);
     }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (null == convertView) {
+                convertView = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+                holder = new ViewHolder();
+                holder.indicator = convertView.findViewById(R.id.indicator);
+                holder.title = (TextView) convertView.findViewById(R.id.title);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.indicator.setBackgroundColor(COLORS[position]);
+            holder.title.setText(TITLES[position]);
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+        View indicator;
+        TextView title;
+    }
+
 }

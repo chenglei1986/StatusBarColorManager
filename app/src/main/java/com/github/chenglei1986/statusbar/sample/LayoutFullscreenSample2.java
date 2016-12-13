c2460d2457149dedfa9b62f7daf21ec6955e5ad3
chenglei <chenglei.1986@163.com>
2016-12-13 14:59:16 +0800

    modify sample

--- app/src/main/java/com/github/chenglei1986/statusbar/sample/LayoutFullscreenSample2.java
+++ app/src/main/java/com/github/chenglei1986/statusbar/sample/LayoutFullscreenSample2.java
@@ -2,17 +2,12 @@ package com.github.chenglei1986.statusbar.sample;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 
-import com.github.chenglei1986.statusbar.ColorUtil;
 import com.github.chenglei1986.statusbar.StatusBarColorManager;
-
-import java.util.List;
 
 public class LayoutFullscreenSample2 extends AppCompatActivity {
 
@@ -28,23 +23,6 @@ public class LayoutFullscreenSample2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_fullscreen_sample2);
         mStatusBarColorManager = new StatusBarColorManager(this);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.sample2);
-        Palette.Builder builder = Palette.from(bitmap);
-        builder.generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(Palette palette) {
-                List<Palette.Swatch> swatchList = palette.getSwatches();
-                for (Palette.Swatch swatch : swatchList) {
-                    if (swatch != null) {
-                        int color = swatch.getRgb();
-                        if (ColorUtil.isLightColor(color)) {
-                            mStatusBarColorManager.setStatusBarColor(swatch.getRgb(), true, false);
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-
+        mStatusBarColorManager.setStatusBarColor(Color.WHITE, true, false);
     }
 }

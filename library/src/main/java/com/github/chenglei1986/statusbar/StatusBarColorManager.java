a58797d427200e92c43c2a3d72544667412b8bf4
chenglei <chenglei.1986@163.com>
2017-02-03 16:33:03 +0800

    Refactoring

--- library/src/main/java/com/github/chenglei1986/statusbar/StatusBarColorManager.java
+++ library/src/main/java/com/github/chenglei1986/statusbar/StatusBarColorManager.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Build;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,66 +12,128 @@ import android.widget.FrameLayout;
 
 public class StatusBarColorManager {
 
-    private Activity mActivity;
-    private ViewGroup mDecorView;
-    private FrameLayout mStatusBarBackground;
-    private int mStatusBarHeight;
-    private int mActionBarHeight;
-    private ViewGroup mContentView;
+    private interface StatusBarColorManagerImpl {
+        void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar);
+    }
 
-    public StatusBarColorManager(@NonNull Activity activity) {
-        mActivity = activity;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+    private static class StatusBarColorManagerBase implements StatusBarColorManagerImpl {
+
+        @Override
+        public void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private static class StatusBarColorManagerImplApi19 extends StatusBarColorManagerBase {
+
+        protected Activity mActivity;
+        protected ViewGroup mDecorView;
+        protected FrameLayout mStatusBarBackground;
+        protected int mStatusBarHeight;
+        protected int mActionBarHeight;
+        protected ViewGroup mContentView;
+
+        public StatusBarColorManagerImplApi19(@NonNull Activity activity) {
+            mActivity = activity;
             StatusBarUtil.setTranslucentStatus(mActivity);
 
             mDecorView = (ViewGroup) activity.getWindow().getDecorView();
             mContentView = (ViewGroup) ((ViewGroup) mDecorView.findViewById(android.R.id.content)).getChildAt(0);
-            if (null == mStatusBarBackground) {
-                mStatusBarBackground = new FrameLayout(activity);
-            }
             mStatusBarHeight = StatusBarUtil.getStatusHeight(activity);
             mActionBarHeight = StatusBarUtil.getActionBarHeight(activity);
+            if (null == mStatusBarBackground) {
+                mStatusBarBackground = new FrameLayout(activity);
+                mDecorView.addView(mStatusBarBackground, ViewGroup.LayoutParams.MATCH_PARENT, mStatusBarHeight);
+            }
+        }
+
+        protected void setStatusBarColorLight(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+            setColorAndContentPadding(color, layoutFullscreen, withActionBar, true);
+        }
+
+        protected void setStatusBarColorDark(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+            setColorAndContentPadding(color, layoutFullscreen, withActionBar, false);
+        }
+
+        private void setColorAndContentPadding(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar, boolean light) {
+            if (BrandUtil.checkBrand(BrandUtil.BRAND.XIAOMI)) {
+                StatusBarUtil.setMiuiStatusBarIconDarkMode(mActivity, light);
+            } else if (BrandUtil.checkBrand(BrandUtil.BRAND.MEIZU)) {
+                StatusBarUtil.setFlymeStatusBarIconDarkMode(mActivity, light);
+            }
+            int contentViewPaddingTop = (layoutFullscreen ? 0 : mStatusBarHeight) + (withActionBar ? mActionBarHeight : 0);
+            mContentView.setPadding(mContentView.getPaddingLeft(), contentViewPaddingTop, mContentView.getPaddingRight(), mContentView.getPaddingBottom());
+            mStatusBarBackground.setBackgroundColor(layoutFullscreen ? 0x00000000 : color);
+        }
+
+        @Override
+        public void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+            if (ColorUtil.isLightColor(color)) {
+                setStatusBarColorLight(color, layoutFullscreen, withActionBar);
+            } else {
+                setStatusBarColorDark(color, layoutFullscreen, withActionBar);
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private static class StatusBarColorManagerImplApi21 extends StatusBarColorManagerImplApi19 {
+
+        public StatusBarColorManagerImplApi21(@NonNull Activity activity) {
+            super(activity);
+        }
+
+        @Override
+        public void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+            mStatusBarBackground.removeAllViews();
+            if (ColorUtil.isLightColor(color)) {
+                setStatusBarColorLight(color, layoutFullscreen, withActionBar);
+                View scrimView = new View(mActivity);
+                scrimView.setBackground(ScrimUtil.makeCubicGradientScrimDrawable(0xFF000000, 8, Gravity.TOP));
+                mStatusBarBackground.addView(scrimView);
+            } else {
+                setStatusBarColorDark(color, layoutFullscreen, withActionBar);
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static class StatusBarColorManagerImplApi23 extends StatusBarColorManagerImplApi21 {
+
+        public StatusBarColorManagerImplApi23(@NonNull Activity activity) {
+            super(activity);
+        }
+
+        @Override
+        public void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
+            mStatusBarBackground.removeAllViews();
+            if (ColorUtil.isLightColor(color)) {
+                setStatusBarColorLight(color, layoutFullscreen, withActionBar);
+                mDecorView.setSystemUiVisibility(mDecorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            } else {
+                setStatusBarColorDark(color, layoutFullscreen, withActionBar);
+                mDecorView.setSystemUiVisibility(mDecorView.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+    }
+
+    private StatusBarColorManagerImpl mStatusBarColorManagerImpl;
+
+    public StatusBarColorManager(@NonNull Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mStatusBarColorManagerImpl = new StatusBarColorManagerImplApi23(activity);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mStatusBarColorManagerImpl = new StatusBarColorManagerImplApi21(activity);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mStatusBarColorManagerImpl = new StatusBarColorManagerImplApi19(activity);
+        } else {
+            mStatusBarColorManagerImpl = new StatusBarColorManagerBase();
         }
     }
 
     public void setStatusBarColor(@ColorInt int color, boolean layoutFullscreen, boolean withActionBar) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            return;
-        }
-        mDecorView.removeView(mStatusBarBackground);
-        mStatusBarBackground.removeAllViews();
-        if (ColorUtil.isLightColor(color)) {
-            boolean darkMode = false;
-            if (BrandUtil.checkBrand(BrandUtil.BRAND.XIAOMI)) {
-                darkMode = StatusBarUtil.setMiuiStatusBarIconDarkMode(mActivity, true);
-            } else if (BrandUtil.checkBrand(BrandUtil.BRAND.MEIZU)) {
-                darkMode = StatusBarUtil.setFlymeStatusBarIconDarkMode(mActivity, true);
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-                if (!darkMode) {
-                    View scrimView = new View(mActivity);
-                    scrimView.setBackground(ScrimUtil.makeCubicGradientScrimDrawable(0xFF000000, 8, Gravity.TOP));
-                    mStatusBarBackground.addView(scrimView);
-                }
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                mDecorView.setSystemUiVisibility(mDecorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            }
-
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                mDecorView.setSystemUiVisibility(mDecorView.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            }
-            if (BrandUtil.checkBrand(BrandUtil.BRAND.XIAOMI)) {
-                StatusBarUtil.setMiuiStatusBarIconDarkMode(mActivity, false);
-            } else if (BrandUtil.checkBrand(BrandUtil.BRAND.MEIZU)) {
-                StatusBarUtil.setFlymeStatusBarIconDarkMode(mActivity, false);
-            }
-        }
-        int contentViewPaddingTop = (layoutFullscreen ? 0 : mStatusBarHeight) + (withActionBar ? mActionBarHeight : 0);
-        mContentView.setPadding(mContentView.getPaddingLeft(), contentViewPaddingTop, mContentView.getPaddingRight(), mContentView.getPaddingBottom());
-        mStatusBarBackground.setBackgroundColor(layoutFullscreen ? 0x00000000 : color);
-        mDecorView.addView(mStatusBarBackground, ViewGroup.LayoutParams.MATCH_PARENT, mStatusBarHeight);
+        mStatusBarColorManagerImpl.setStatusBarColor(color, layoutFullscreen, withActionBar);
     }
 
 }
--- library/src/main/java/com/github/chenglei1986/statusbar/StatusBarUtil.java
+++ library/src/main/java/com/github/chenglei1986/statusbar/StatusBarUtil.java
@@ -88,13 +88,11 @@ public class StatusBarUtil {
     }
 
     public static void setTranslucentStatus(Activity activity) {
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
         }
-        if (Build.VERSION.SDK_INT >= 19) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
             activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
